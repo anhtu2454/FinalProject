e9ae066a0b87c0c05b1e6e01bcf7353d2b3c6ea3
anhtu2454 <anhtu2454@gmail.com>
2026-08-21 14:24:00 +0700

    Update map and plane controller

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target; // kéo máy bay (LightPlane) vào đây
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0.08f, 5.45f); // khoảng cách camera so với máy bay
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        transform.position = target.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/Firecontroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    private ParticleSystem fire;
+
+    void Awake()
+    {
+        fire = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void Extinguish()
+    {
+        if (fire != null)
+        {
+            fire.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+
+        Destroy(gameObject);
+    }
+}
